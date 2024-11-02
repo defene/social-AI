150cc6d4ca209293c9440efb38a3f2d0099e5ffc
defene <cyh_20@mail.ustc.edu.cn>
2024-11-02 20:33:22 +0000

    be ready with POST

---
+++ src/socialai/handler/post.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"socialai/model"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Received one upload request")
+
+	decoder := json.NewDecoder(r.Body)
+	var p model.Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
